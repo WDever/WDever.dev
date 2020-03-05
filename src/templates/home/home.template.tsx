a71a18b68ce293ad0e1e7154708a19e4d8bc7caf
WDever <alsrbtkfkd7@gmail.com>
2020-03-05 23:32:14 +0900

    feat: Home template posts filter by tags add includesArr util function

--- src/templates/home/home.template.tsx
+++ src/templates/home/home.template.tsx
@@ -1,20 +1,25 @@
 import React, { ReactElement } from 'react';
 import { BlogIndexQueryQuery } from 'types';
-import PostItemComponent from 'components/items/postItem';
+import PostItemComponent from 'components/items/post-item';
+import { includesArr } from 'utils';
 import { Main, Inner } from './home.style';
 
 interface Props {
   posts: BlogIndexQueryQuery['allMarkdownRemark']['edges'];
+  selectedTags: string[];
 }
 
-export default function HomeTemplate({ posts }: Props): ReactElement {
-  const postList = posts.map(
+export default function HomeTemplate({
+  posts,
+  selectedTags,
+}: Props): ReactElement {
+  const allPostList = posts.map(
     ({
       node,
     }: {
       node: BlogIndexQueryQuery['allMarkdownRemark']['edges'][number]['node'];
     }) => {
-      const title = node.frontmatter.title || node.fields.slug;
+      const title: string = node.frontmatter.title || node.fields.slug;
 
       return (
         <PostItemComponent
@@ -29,6 +34,36 @@ export default function HomeTemplate({ posts }: Props): ReactElement {
       );
     },
   );
+
+  const filteredPostList = posts
+    .map(
+      ({
+        node,
+      }: {
+        node: BlogIndexQueryQuery['allMarkdownRemark']['edges'][number]['node'];
+      }) => {
+        const title: string = node.frontmatter.title || node.fields.slug;
+
+        if (includesArr(node.frontmatter.tags, selectedTags)) {
+          return (
+            <PostItemComponent
+              key={node.fields.slug}
+              img='https://miro.medium.com/max/10368/1*RF2MAfjB1_0FowzOmTW78Q.jpeg'
+              title={title}
+              date={node.frontmatter.date}
+              excerpt={node.excerpt}
+              tags={node.frontmatter.tags}
+              slug={node.fields.slug}
+            />
+          );
+        }
+
+        return undefined;
+      },
+    )
+    .filter(item => item !== undefined);
+
+  const postList = selectedTags.length === 0 ? allPostList : filteredPostList;
 
   return (
     <Main>
