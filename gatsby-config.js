ab19460b81315a9d10045c4ba919f33a980d7fbe
WDever <alsrbtkfkd7@gmail.com>
2020-05-03 17:19:31 +0900

    fix: Post nav error when no prev & next post

--- gatsby-config.js
+++ gatsby-config.js
@@ -16,8 +16,8 @@ module.exports = {
     {
       resolve: `gatsby-source-filesystem`,
       options: {
-        path: `${__dirname}/content/blog`,
-        name: `blog`,
+        path: `${__dirname}/content/articles`,
+        name: `articles`,
       },
     },
     {
